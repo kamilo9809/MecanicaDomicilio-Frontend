cdf48c74a9ce486fc5ad73c353910dfe26de44c9
tomasherreradev <herreratomas270@gmail.com>
2024-02-28 19:38:32 -0300

    configuración de enrutamiento y componentes generales (head, foot y slider)

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
 import { GeneralHeaderComponent } from './../componentes-generales/general-header/general-header.component';
+import { SliderMarcasComponent } from './../componentes-generales/slider-marcas/slider-marcas.component';
+import { GeneralFooterComponent } from './../componentes-generales/general-footer/general-footer.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [GeneralHeaderComponent],
+  imports: [GeneralHeaderComponent, GeneralFooterComponent, SliderMarcasComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
