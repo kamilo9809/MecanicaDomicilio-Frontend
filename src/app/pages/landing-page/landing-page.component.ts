4796485a15dc77260dee184d839064450d540e49
tomasherreradev <herreratomas270@gmail.com>
2024-02-29 19:55:08 -0300

    primeros tres componentes del landing

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 
 import { GeneralHeaderComponent } from './../componentes-generales/general-header/general-header.component';
+import { LandingComp1Component } from './subcomponentes-landing-page/landing-comp-1/landing-comp-1.component';
 import { SliderMarcasComponent } from './../componentes-generales/slider-marcas/slider-marcas.component';
-import { GeneralFooterComponent } from './../componentes-generales/general-footer/general-footer.component';
-import { CardProductComponent } from './../componentes-generales/card-product/card-product.component';
+import { LandingComp2Component } from './subcomponentes-landing-page/landing-comp-2/landing-comp-2.component';
+import { LandingComp3Component } from './subcomponentes-landing-page/landing-comp-3/landing-comp-3.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [GeneralHeaderComponent, GeneralFooterComponent, SliderMarcasComponent, CardProductComponent],
+  imports: [
+    GeneralHeaderComponent, 
+    LandingComp1Component, 
+    SliderMarcasComponent, 
+    LandingComp2Component,
+    LandingComp3Component
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
